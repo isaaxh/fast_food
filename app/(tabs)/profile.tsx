d3b2e4da4bc1710d7aaa56b0414fa94d460bbf53
Isaac Hussain <isaaclearns58@gmail.com>
2025-07-20 22:38:07 +0300

    refactor: implement flatlist

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -6,11 +6,42 @@ import { images } from "@/constants";
 import { signOut } from "@/lib/appwrite";
 import useAuthStore from "@/store/auth.store";
 import React from "react";
-import { Image, View } from "react-native";
+import { FlatList, Image, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Profile = () => {
   const { fetchAuthenticatedUser, user } = useAuthStore();
+
+  const profileItemList = [
+    {
+      label: "Full Name",
+      value: user?.name,
+      icon: images.user,
+    },
+
+    {
+      label: "Email",
+      value: user?.email,
+      icon: images.envelope,
+    },
+
+    {
+      label: "Phone Number",
+      value: "+966 55 790 6658",
+      icon: images.phone,
+    },
+
+    {
+      label: "Address 1 - (Home)",
+      value: "Omar Al Kadi St., Sharaie No.4, Mecca",
+      icon: images.location,
+    },
+    {
+      label: "Address 2 - (Work)",
+      value: "Abdallah Bin Abbas St., Shoqiya, Mecca",
+      icon: images.location,
+    },
+  ];
 
   const logout = async () => {
     try {
@@ -22,92 +53,57 @@ const Profile = () => {
   };
 
   return (
-    <SafeAreaView className='px-5 pt-5 pb-28'>
-      <CustomHeader title='Profile' />
-
-      <View className='items-center'>
-        <Avatar imgUrl={images.avatar} />
-      </View>
-
-      <View className='px-12 mb-12 gap-y-6'>
-        <ProfileInfoItem
-          label='Full Name'
-          value={user?.name ?? ""}
-          icon={
-            <Image
-              source={images.user}
-              className='size-6'
-              resizeMode='contain'
+    <SafeAreaView className='px-5 pt-5 pb-20'>
+      <FlatList
+        data={profileItemList}
+        renderItem={({ item }) => (
+          <View className='px-10'>
+            <ProfileInfoItem
+              label={item.label}
+              value={item.value ?? ""}
+              icon={
+                <Image
+                  source={item.icon}
+                  className='size-6'
+                  resizeMode='contain'
+                />
+              }
             />
-          }
-        />
-        <ProfileInfoItem
-          label='Email'
-          value={user?.email ?? ""}
-          icon={
-            <Image
-              source={images.envelope}
-              className='size-6'
-              resizeMode='contain'
+          </View>
+        )}
+        contentContainerClassName='mb-12 gap-y-5'
+        ListHeaderComponent={() => (
+          <>
+            <CustomHeader title='Profile' />
+            <View className='items-center'>
+              <Avatar imgUrl={images.avatar} />
+            </View>
+          </>
+        )}
+        ListFooterComponent={() => (
+          <View className='mt-10 gap-y-5'>
+            <CustomButton
+              title='Edit Profile'
+              variant='secondary'
+              onPress={() => {}}
             />
-          }
-        />
-        <ProfileInfoItem
-          label='Phone Number'
-          value='+966 50 285 0922'
-          icon={
-            <Image
-              source={images.phone}
-              className='size-6'
-              resizeMode='contain'
+            <CustomButton
+              title='Log Out'
+              variant='secondary'
+              style='border-red-600'
+              textStyle='text-red-600'
+              leftIcon={
+                <Image
+                  source={images.logout}
+                  resizeMode='contain'
+                  className='mr-2 size-6'
+                />
+              }
+              onPress={logout}
             />
-          }
-        />
-        <ProfileInfoItem
-          label='Address 1 - (Home)'
-          value='Omar Al Kadi St., Sharaie No.4, Mecca'
-          icon={
-            <Image
-              source={images.location}
-              className='size-6'
-              resizeMode='contain'
-            />
-          }
-        />
-        <ProfileInfoItem
-          label='Address 2 - (Work)'
-          value='Abdallah Bin Abbas St., Shoqiya, Mecca'
-          icon={
-            <Image
-              source={images.location}
-              className='size-6'
-              resizeMode='contain'
-            />
-          }
-        />
-      </View>
-
-      <View className='gap-y-5'>
-        <CustomButton
-          title='Edit Profile'
-          variant='secondary'
-          onPress={() => {}}
-        />
-        <CustomButton
-          title='Log Out'
-          variant='secondary'
-          style='border-red-600'
-          textStyle='text-red-600'
-          leftIcon={
-            <Image
-              source={images.logout}
-              resizeMode='contain'
-              className='mr-2 size-6'
-            />
-          }
-          onPress={logout}
-        />
-      </View>
+          </View>
+        )}
+      />
     </SafeAreaView>
   );
 };
